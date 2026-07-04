70d717d2ee718a5e6743d15117fd2b8e6d0cb23b
Asheer Hadayat <abbasihashirabbasi0@gmail.com>
2026-07-04 21:53:27 +0500

    Few Updates

--- MediLink_Backend/server.js
+++ MediLink_Backend/server.js
@@ -31,9 +31,13 @@ const app = express();
 
 // ----- MIDDLEWARE -----
 app.use(helmet()); // basic security headers
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:3000",
+    ],
     credentials: true,
   })
 );
